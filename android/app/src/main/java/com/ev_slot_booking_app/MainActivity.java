53969f968a87e4971f9a0abe605b57b55c1f9b11
AdithyaSanyal <adithyasanyal@gmail.com>
2021-11-28 13:33:46 +0530

    Initialized repo and folder structure

---
+++ android/app/src/main/java/com/ev_slot_booking_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ev_slot_booking_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "EV_Slot_Booking_App";
+  }
+}
