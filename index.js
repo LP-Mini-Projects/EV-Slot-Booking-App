f52b295453c3758b2b0dddee4b84bf9774ef1692
priyambi <priyambi.hiran@gmail.com>
2021-11-29 15:44:58 +0530

    Splash Screen Updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Splash from './src/screens/Splash';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Splash);
--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,1 +1,38 @@
-hi
+import React from 'react';
+import {StyleSheet, View, Image} from 'react-native';
+
+import LottieView from 'lottie-react-native';
+
+export default function Splash() {
+  return (
+    <View style={styles.container}>
+      <LottieView
+        source={require('../assets/splash1.json')}
+        autoPlay={true}
+        loop
+        style={styles.animation}
+      />
+      <Image source={require('../assets/logo.png')} style={styles.image} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1f2128',
+  },
+  animation: {
+    width: 200,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 120,
+    marginLeft: 60,
+  },
+  image: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 110,
+  },
+});
