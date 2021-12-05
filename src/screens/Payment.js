36350ffef2aafc9490d6f6840f62cc0735093b38
priyambi <priyambi.hiran@gmail.com>
2021-12-05 16:01:09 +0530

    Booking Screen Design issue resolved

--- src/screens/Payment.js
+++ src/screens/Payment.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text, Image, Button} from 'react-native';
-
+import {StyleSheet, View, Text, Image} from 'react-native';
 export default function Payment() {
   return (
     <View style={styles.container}>
